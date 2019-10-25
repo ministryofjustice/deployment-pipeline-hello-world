d5a723cc40d5a86581e0a1b48ee5f61c56abb72c
David Elliott <david.elliott@digital.justice.gov.uk>
2019-10-25 15:24:50 +0100

    Add assume role for terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,14 +4,28 @@ terraform {
     region               = "eu-west-2"
     key                  = "ccmsdeploymentspike.tfstate"
     workspace_key_prefix = "laa-platform"
-    profile              = "laa-shared-services"
+    role_arn             = "arn:aws:iam::902837325998:role/CircleCi"
   }
 }
 
 provider "aws" {
   region = var.region
+
+  assume_role {
+    role_arn     = "arn:aws:iam::411213865113:role/Terraform"
+  }
+}
+
+provider "aws" {
+  region = var.region
+  alias  = "shared-services"
+
+  assume_role {
+    role_arn     = "arn:aws:iam::902837325998:role/TerraformRole"
+  }
 }
 
 resource "aws_ecr_repository" "ccms_deployment_spike" {
   name = "laa-ccms-deployment-spike"
+  provider = aws.shared-services
 }
