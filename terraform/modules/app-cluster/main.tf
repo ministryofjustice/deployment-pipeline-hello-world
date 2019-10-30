f4f3d5436eb5bda114a4969edc5984d4eb137b3d
Bethan Palmer <bethan.palmer@digital.justice.gov.uk>
2019-10-30 14:35:31 +0000

    Add a load balancer so that we can access the application

--- terraform/modules/app-cluster/main.tf
+++ terraform/modules/app-cluster/main.tf
@@ -38,7 +38,7 @@ resource "aws_ecs_task_definition" "app" {
   memory                = var.fargate_memory
   container_definitions = data.template_file.hello_world_app.rendered
 
-//  tags = local.default_tags
+  //tags = local.default_tags
 }
 
 # This ensures we have the required number of containers running
@@ -61,7 +61,14 @@ resource "aws_ecs_service" "main" {
     assign_public_ip = true
   }
 
+  load_balancer {
+    target_group_arn = aws_alb_target_group.app.id
+    container_name   = "hello-world"
+    container_port   = var.app_port
+  }
+
   depends_on = [
+    aws_alb_listener.front_end,
     aws_iam_role_policy_attachment.ecs_task_execution_role,
   ]
 
